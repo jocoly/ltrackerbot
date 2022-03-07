b4c606fac1eb4e27f62530be80bd31f9eba01d56
jocoly <josephlyell@gmail.com>
2022-03-07 16:59:06 -0500

    Removed sample, added W + L messages

--- l-tracker-bot-discord/main.go
+++ l-tracker-bot-discord/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -19,7 +15,8 @@ var (
 	Token string
 )
 
-const KuteGoAPIURL = "http://localhost:8080"
+const LMessage = " has taken another L"
+const WMessage = " has secured another W"
 
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
@@ -58,10 +55,6 @@ func main() {
 	dg.Close()
 }
 
-type Gopher struct {
-	Name string `json: "name"`
-}
-
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -72,80 +65,13 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == "!gopher" {
-
-		//Call the KuteGo API and retrieve our cute Dr Who Gopher
-		response, err := http.Get(KuteGoAPIURL + "/gopher/" + "dr-who")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			_, err = s.ChannelFileSend(m.ChannelID, "dr-who.png", response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get dr-who Gopher! :-(")
-		}
+	if m.Content == "!L" && m.Type == discordgo.MessageTypeReply {
+		referencedAuthor := m.ReferencedMessage.Author.Username
+		s.ChannelMessageSend(m.ChannelID, referencedAuthor+LMessage)
 	}
 
-	if m.Content == "!random" {
-
-		//Call the KuteGo API and retrieve a random Gopher
-		response, err := http.Get(KuteGoAPIURL + "/gopher/random/")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			_, err = s.ChannelFileSend(m.ChannelID, "random-gopher.png", response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get random Gopher! :-(")
-		}
-	}
-
-	if m.Content == "!gophers" {
-
-		//Call the KuteGo API and display the list of available Gophers
-		response, err := http.Get(KuteGoAPIURL + "/gophers/")
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer response.Body.Close()
-
-		if response.StatusCode == 200 {
-			// Transform our response to a []byte
-			body, err := ioutil.ReadAll(response.Body)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// Put only needed informations of the JSON document in our array of Gopher
-			var data []Gopher
-			err = json.Unmarshal(body, &data)
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			// Create a string with all of the Gopher's name and a blank line as separator
-			var gophers strings.Builder
-			for _, gopher := range data {
-				gophers.WriteString(gopher.Name + "\n")
-			}
-
-			// Send a text message with the list of Gophers
-			_, err = s.ChannelMessageSend(m.ChannelID, gophers.String())
-			if err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			fmt.Println("Error: Can't get list of Gophers! :-(")
-		}
+	if m.Content == "!W" && m.Type == discordgo.MessageTypeReply {
+		referencedAuthor := m.ReferencedMessage.Author.Username
+		s.ChannelMessageSend(m.ChannelID, referencedAuthor+WMessage)
 	}
 }
