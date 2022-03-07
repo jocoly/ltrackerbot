9806073599fdc933f26f48cb58dce22f154e6c0c
jocoly <62028785+jocoly@users.noreply.github.com>
2022-03-07 17:00:54 -0500

    Delete l-tracker-bot-discord/kutego-api-main directory

--- l-tracker-bot-discord/kutego-api-main/pkg/swagger/gen.go
+++
@@ -1,5 +0,0 @@
-package swagger
-
-//go:generate rm -rf server
-//go:generate mkdir -p server
-//go:generate swagger generate server --quiet --target server --name kutego-api --spec swagger.yml --exclude-main
