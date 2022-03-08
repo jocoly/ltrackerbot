469fc39550c0cbef4044d3d044867f29e9004088
jocoly <josephlyell@gmail.com>
2022-03-07 19:55:12 -0500

    functions

--- l-tracker-bot-discord/main.go
+++ l-tracker-bot-discord/main.go
@@ -1,3 +1,6 @@
+//build
+//export BOT_TOKEN=<TOKEN GOES HERE!!!>
+//go run main.go -t $BOT_TOKEN
 package main
 
 import (
@@ -15,13 +18,17 @@ var (
 	Token string
 )
 
-const LMessage = " has taken another L"
-const WMessage = " has secured another W"
-
 func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
+
+const LMessage = " has taken another L"
+const WMessage = " has secured another W"
+
+// var userID = -1
+// var lCount = -1
+// var wCount = -1
 
 func main() {
 
@@ -34,8 +41,8 @@ func main() {
 
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
+	// dg.AddHandler(LWCount)
 
-	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
 	// Open a websocket connection to Discord and begin listening.
@@ -60,18 +67,54 @@ func main() {
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
 	if m.Content == "!L" && m.Type == discordgo.MessageTypeReply {
 		referencedAuthor := m.ReferencedMessage.Author.Username
-		s.ChannelMessageSend(m.ChannelID, referencedAuthor+LMessage)
+		if referencedAuthor != "" {
+			s.ChannelMessageSend(m.ChannelID, referencedAuthor+LMessage)
+		} else {
+			return
+		}
 	}
 
 	if m.Content == "!W" && m.Type == discordgo.MessageTypeReply {
 		referencedAuthor := m.ReferencedMessage.Author.Username
-		s.ChannelMessageSend(m.ChannelID, referencedAuthor+WMessage)
+		if referencedAuthor != "" {
+			s.ChannelMessageSend(m.ChannelID, referencedAuthor+WMessage)
+		} else {
+			return
+		}
 	}
 }
+
+func LWCount(s *discordgo.Session) {
+	//guildID := s.Guild
+
+	//slice1 = guild members by userID
+	//slice2 = LCount values where index position corresponds to a guild member
+	//slice3 = WCount values where index position corresponds to a guild member
+	//initializes with members and 0s for counts when the bot is first booted up
+	//appends new guild member + 0s when a new user joins
+	//returns the 3 slices
+}
+
+//func fetchLCount(userID, slice1, slice2)
+//finds userID in slice1 and stores its index position
+//returns slice2 value at that index position
+
+//func fetchWCount(userID, slice1, slice3)
+//finds userID in slice1 and stores its index position
+//returns slice3 value at that index position
+
+//func firstLCheck(userID, slice1, slice2)
+//runs when !L is replied to a message
+//if fetchLCount == 0, true, trigger flavor text
+//else false, trigger standard text
+
+//func firstWCheck(userID, slice1, slice3)
+//runs when !L is replied to a message
+//if fetchWCount == 0, true, trigger flavor text
+//else false, trigger standard text
